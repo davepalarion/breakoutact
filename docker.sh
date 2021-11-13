b1f3fa8e741fb5411b6a7246af67402620ff6684
NetAcad DEVASC <changeme@example.com>
2021-11-13 12:05:51 +0000

    Changed port from 8080 to 5050.

--- docker.sh
+++ docker.sh
@@ -13,10 +13,10 @@ echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "RUN pip install ipapi" >> tempdir/Dockerfile
 echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY  ipflask.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
 echo "CMD python3 /home/myapp/ipflask.py" >> tempdir/Dockerfile
 
 cd tempdir
 docker build -t projectapp .
-docker run -t -d -p 8080:8080 --name projectrunning projectapp
+docker run -t -d -p 5050:5050 --name projectrunning projectapp
 docker ps -a
